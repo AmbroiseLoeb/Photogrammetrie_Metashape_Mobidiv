3fe71c333a162fd109760a762d6e99ef45906f14
aloeb <ambroise.loeb@inrae.fr>
2024-02-19 14:35:40 +0100

    reformat the file

--- test_script_metashape.py
+++ test_script_metashape.py
@@ -3,14 +3,15 @@ import Metashape
 
 #  création du chunk
 doc = Metashape.app.document
-chk =doc.chunk
+chk = doc.chunk
 if not chk:
-    chk =doc.addChunk()
+    chk = doc.addChunk()
 
 # importation des photos
-path_dossier = r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\Session 2023-03-08 12-45-21\uplot_100_1" 
-liste_images = (r"\uplot_100_camera_1_1_RGB.jpg" , r"\uplot_100_camera_2_1_RGB.jpg", r"\uplot_100_camera_1_2_RGB.jpg", r"\uplot_100_camera_2_2_RGB.jpg")
-for image in liste_images :
+path_dossier = r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\Session 2023-03-08 12-45-21\uplot_100_1"
+liste_images = (r"\uplot_100_camera_1_1_RGB.jpg", r"\uplot_100_camera_2_1_RGB.jpg", r"\uplot_100_camera_1_2_RGB.jpg",
+                r"\uplot_100_camera_2_2_RGB.jpg")
+for image in liste_images:
     path_image = str(path_dossier + image)
     chk.addPhotos(path_image)
 
@@ -20,7 +21,7 @@ chk.alignCameras()
 
 # construction du nuage de point
 chk.buildDepthMaps(downscale=2, filter_mode=Metashape.NoFiltering, reuse_depth=True)
-chk.buildPointCloud(point_colors=True, point_confidence=True,keep_depth=True, max_neighbors=16)
+chk.buildPointCloud(point_colors=True, point_confidence=True, keep_depth=True, max_neighbors=16)
 
 # construction du model 3D
 chk.buildModel(surface_type=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation)
@@ -31,4 +32,3 @@ chk.buildTexture(blending_mode=Metashape.MosaicBlending, texture_size=4096)
 
 # enregistrement du projet metashape
 doc.save(r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\test3.psx")
-
