bfc38208fd284bb4d5d97b2855c7fd9f05120058
aloeb-GH <ambroise.loeb@inrae.fr>
2024-03-26 11:16:09 +0100

    update : subprocess + save + export fonctionne depuis python

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import subprocess
 def main():
     """..."""
     # Lancer Metashape
-    subprocess.run(["C:\Program Files\Agisoft\Metashape Pro\metashape.exe", "test_script_metashape.py"])
+    subprocess.run([r'C:\Program Files\Agisoft\Metashape Pro\metashape.exe', '-r', r'test_script_metashape.py'])
 
     # Traiter les résultats
 
 
 if __name__ == "__main__":
     main()
+
--- test_script_metashape.py
+++ test_script_metashape.py
@@ -1,18 +1,36 @@
 # importation librairies
 import Metashape
 
-#  création du chunk
-doc = Metashape.app.document
-chk =doc.chunk
+#  creation du doc
+doc = Metashape.Document()
+
+# enregistrement du projet
+# path = Metashape.app.getSaveFileName("Save Project As")
+path = r'C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\export\retest.psx'
+try:
+    doc.save(path)
+except RuntimeError:
+    Metashape.app.messageBox("Can't save project")
+    print("Can't save project")
+
+#  creation du chunk
+chk = doc.chunk
 if not chk:
-    chk =doc.addChunk()
+    chk = doc.addChunk()
 
 # importation des photos
-path_dossier = r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\Session 2023-03-08 12-45-21\uplot_100_1" 
-liste_images = (r"\uplot_100_camera_1_1_RGB.jpg" , r"\uplot_100_camera_2_1_RGB.jpg", r"\uplot_100_camera_1_2_RGB.jpg", r"\uplot_100_camera_2_2_RGB.jpg")
-for image in liste_images :
+path_dossier = r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\Session 2023-03-08 12-45-21\uplot_100_1"
+liste_images = (r"\uplot_100_camera_1_1_RGB.jpg", r"\uplot_100_camera_2_1_RGB.jpg",
+                r"\uplot_100_camera_1_2_RGB.jpg", r"\uplot_100_camera_2_2_RGB.jpg")
+for image in liste_images:
     path_image = str(path_dossier + image)
     chk.addPhotos(path_image)
+
+# creation reference distance (avec distance camera)
+chk.addScalebar(chk.cameras[0], chk.cameras[2])
+chk.scalebars[0].label = "dist_cam_ref"
+chk.scalebars[0].reference.distance = 0.06511302126767482
+chk.updateTransform()
 
 # alignement des photos
 chk.matchPhotos(downscale=1, generic_preselection=True, reference_preselection=False)
@@ -20,15 +38,28 @@ chk.alignCameras()
 
 # construction du nuage de point
 chk.buildDepthMaps(downscale=2, filter_mode=Metashape.NoFiltering, reuse_depth=True)
-chk.buildPointCloud(point_colors=True, point_confidence=True,keep_depth=True, max_neighbors=16)
+chk.buildPointCloud(point_colors=True, point_confidence=True, keep_depth=True, max_neighbors=16)
 
-# construction du model 3D
-chk.buildModel(surface_type=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation)
+# filtre par confiance
+chk.point_cloud.setConfidenceFilter(0, 1)
+all_points_classes = list(range(128))
+chk.point_cloud.removePoints(
+    all_points_classes)  # removes all active points of the point cloud, i.e. removing all low-confidence points
+chk.point_cloud.resetFilters()  # resetting filter, so high-confidence points are now active
 
-# avec texture
-chk.buildUV(mapping_mode=Metashape.GenericMapping)
-chk.buildTexture(blending_mode=Metashape.MosaicBlending, texture_size=4096)
+'''
+# filtre par couleur
+gray_color = [84, 66, 52]
+tolerance = 25
+chk.point_cloud.selectPointsByColor(gray_color, tolerance, channels='RGB')
+chk.point_cloud.assignClassToSelection(7)
+'''
 
-# enregistrement du projet metashape
-doc.save(r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\test3.psx")
+doc.save()
+# construction du modele numerique d'elévation (DEM)
+chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.DisabledInterpolation, classes=[0])
 
+# exportation du DEM
+doc.save()
+chk.exportRaster(r"C:\Users\U108-N806\Desktop\Literal_mobidiv_2023\export" + '/DEM_export.tif',
+                 source_data=Metashape.ElevationData)
