efc3f35bde1f047196f4bacba0dca22f1bed3f36
aloeb-GH <ambroise.loeb@inrae.fr>
2024-04-08 17:28:06 +0200

    restructuration : def boucle(path) avec appel de la fonction dans main() + récupérer les DEMs exportées

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import subprocess
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
 
 
-# test lancer metashape depuis python
-
-
+# lancer metashape depuis python
 def main():
 
-    # Lancer Metashape
-    subprocess.run([r'C:\Program Files\Agisoft\Metashape Pro\metashape.exe', '-r', r'test_script_metashape.py'])
+    path_annee = r"C:\Users\U108-N806\Desktop\Comparaison mesures"
+    # path_annee = '/home/loeb/Documents/Comparaison_mesures'
 
-    # Traiter les résultats
+    fonction = "boucle"
+
+    # Executer le script correspondant dans Metashape
+    subprocess.run([r'C:\Program Files\Agisoft\Metashape Pro\metashape.exe', '-r', r'test_script_metashape.py', fonction] + [path_annee])
+
+    # Récupérer les DEMs
+    sessionlist = os.listdir(path_annee)
+    for session in sessionlist:
+        if session.find("Session") == 0:
+            print(session)
+            list_dems = os.listdir(path_annee + "/" + session + "/" + 'DEMs')
+            for file in list_dems:
+                print(file)
+
+                dem = Image.open(path_annee + "/" + session + "/" + 'DEMs' + "/" + file)
+                dem_array = np.array(dem)
+                dem_array[dem_array <= 0] = np.nan
+                dem_array = dem_array * 1000
+                plt.figure() and plt.imshow(dem_array)
+
 
 
 if __name__ == "__main__":
     main()
-
--- test_script_metashape.py
+++ test_script_metashape.py
@@ -1,114 +1,110 @@
 # importation librairies
 import Metashape
 import os
+import sys
 
 
-def workflow(path_dossier, dossier):
-    """traitement des images jusqu'au DEM (digital elevation model)"""
+def boucle(path):
+    def workflow(path_dossier, dossier):
+        """traitement des images jusqu'au DEM (digital elevation model)"""
 
-    chk = doc.addChunk()
+        chk = doc.addChunk()
 
-    liste_images = []
-    for filename in os.listdir(path_dossier + '/' + dossier):
-        # vérifier si le fichier se termine par "RGB.jpg"
-        if filename.endswith("RGB.jpg"):
-            # ajouter le nom du fichier à la liste d'images
-            liste_images.append(filename)
+        liste_images = []
+        for filename in os.listdir(path_dossier + '/' + dossier):
+            # vérifier si le fichier se termine par "RGB.jpg"
+            if filename.endswith("RGB.jpg"):
+                # ajouter le nom du fichier à la liste d'images
+                liste_images.append(filename)
 
-    for image in liste_images:
-        path_image = str(path_dossier + '/' + dossier + '/' + image)
-        chk.addPhotos(path_image)
+        for image in liste_images:
+            path_image = str(path_dossier + '/' + dossier + '/' + image)
+            chk.addPhotos(path_image)
 
-    # creation reference distance (avec distance camera)
-    chk.addScalebar(chk.cameras[0], chk.cameras[2])
-    chk.scalebars[0].label = "dist_cam_ref"
-    chk.scalebars[0].reference.distance = 0.06511302126767482
-    chk.updateTransform()
+        # creation reference distance (avec distance camera)
+        chk.addScalebar(chk.cameras[0], chk.cameras[2])
+        chk.scalebars[0].label = "dist_cam_ref"
+        chk.scalebars[0].reference.distance = 0.06511302126767482
+        chk.updateTransform()
 
-    # alignement des photos
-    chk.matchPhotos(downscale=1, generic_preselection=False, reference_preselection=False)
-    chk.alignCameras()
+        # alignement des photos
+        chk.matchPhotos(downscale=1, generic_preselection=False, reference_preselection=False)
+        chk.alignCameras()
 
-    # construction du nuage de point
-    chk.buildDepthMaps(downscale=2, filter_mode=Metashape.NoFiltering, reuse_depth=True)
-    chk.buildPointCloud(point_colors=True, point_confidence=True, keep_depth=True, max_neighbors=16)
+        # construction du nuage de point
+        chk.buildDepthMaps(downscale=2, filter_mode=Metashape.NoFiltering, reuse_depth=True)
+        chk.buildPointCloud(point_colors=True, point_confidence=True, keep_depth=True, max_neighbors=16)
 
-    '''
-    # filtre par confiance
-    chk.point_cloud.setConfidenceFilter(0, 1)
-    all_points_classes = list(range(128))
-    chk.point_cloud.removePoints(all_points_classes)  # removes all active points of the point cloud, i.e. removing all low-confidence points
-    chk.point_cloud.resetFilters()  # resetting filter, so high-confidence points are now active
-    
-    # filtre par couleur
-    gray_color = [84, 66, 52]
-    tolerance = 25
-    chk.point_cloud.selectPointsByColor(gray_color, tolerance, channels='RGB')
-    chk.point_cloud.assignClassToSelection(7)
-    '''
+        '''
+        # filtre par confiance
+        chk.point_cloud.setConfidenceFilter(0, 1)
+        all_points_classes = list(range(128))
+        chk.point_cloud.removePoints(all_points_classes)  # removes all active points of the point cloud, i.e. removing all low-confidence points
+        chk.point_cloud.resetFilters()  # resetting filter, so high-confidence points are now active
 
-    '''
-    # classification sol
-    chk.point_cloud.classifyGroundPoints(max_angle=30, max_distance=0.01, max_terrain_slope=10, cell_size=0.5)
-    # chk.point_cloud.setClassesFilter(Metashape.Ground)
-    '''
-    chk.transform.matrix = chk.transform.matrix * chk.cameras[0].transform
-    doc.save()
-    # construction du modele numerique d'elévation (DEM)
-    # DEM toutes classes
-    chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.DisabledInterpolation)
-    chk.elevation.label = "Elevation toutes classes"
-    key_allclass = chk.elevation.key
-    """
-    # DEM sol
-    chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.Extrapolated, classes=Metashape.PointClass.Ground)
-    chk.elevation.label = "Elevation sol"
-    key_sol = chk.elevation.key
-    # DEM finale
-    chk.transformRaster(source_data=Metashape.ElevationData, asset=key_allclass, subtract=True, operand_asset=key_sol)
-    chk.elevation.label = "DEM_finale"
-    """
-    label_dem = chk.elevation.label
+        # filtre par couleur
+        gray_color = [84, 66, 52]
+        tolerance = 25
+        chk.point_cloud.selectPointsByColor(gray_color, tolerance, channels='RGB')
+        chk.point_cloud.assignClassToSelection(7)
+        '''
 
-    # exportation du DEM
-    doc.save()
-    chk.exportRaster(path_dossier + '/' + "DEMs" '/' + dossier + label_dem + '_export.tif',
-                     source_data=Metashape.ElevationData)
+        '''
+        # classification sol
+        chk.point_cloud.classifyGroundPoints(max_angle=30, max_distance=0.01, max_terrain_slope=10, cell_size=0.5)
+        # chk.point_cloud.setClassesFilter(Metashape.Ground)
+        '''
+        chk.transform.matrix = chk.transform.matrix * chk.cameras[0].transform
+        doc.save()
+        # construction du modele numerique d'elévation (DEM)
+        # DEM toutes classes
+        chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.DisabledInterpolation)
+        chk.elevation.label = "Elevation toutes classes"
+        key_allclass = chk.elevation.key
+        """
+        # DEM sol
+        chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.Extrapolated, classes=Metashape.PointClass.Ground)
+        chk.elevation.label = "Elevation sol"
+        key_sol = chk.elevation.key
+        # DEM finale
+        chk.transformRaster(source_data=Metashape.ElevationData, asset=key_allclass, subtract=True, operand_asset=key_sol)
+        chk.elevation.label = "DEM_finale"
+        """
+        label_dem = chk.elevation.label
+
+        # exportation du DEM
+        doc.save()
+        chk.exportRaster(path_dossier + '/' + "DEMs" '/' + dossier + label_dem + '_export2.tif',
+                         source_data=Metashape.ElevationData)
+
+    #  creation du doc
+    doc = Metashape.Document()
+
+    # enregistrement du projet
+    # path = Metashape.app.getSaveFileName("Save Project As")
+    path_project = path + r'\Export2.psx'
+    try:
+        doc.save(path_project)
+    except RuntimeError:
+        Metashape.app.messageBox("Can't save project")
+        print("Can't save project")
+
+    # importation des photos
+    # PATH = r"C:\Users\U108-N806\Desktop\Comparaison mesures"
+    sessionlist = os.listdir(path)
+    for session in sessionlist:
+        if session.find("Session") == 0:
+            print(session)
+            plotlist = os.listdir(path + "/" + session)
+            if not os.path.exists(path + "/" + session + "/" + "DEMs"):
+                # Crée le fichier s'il n'existe pas
+                os.makedirs(path + "/" + session + "/" + "DEMs")
+            for plot in plotlist:
+                if plot.find("uplot") == 0:
+                    print(plot)
+                    path_session = path + "/" + session
+                    print(path_session)
+                    workflow(path_session, plot)
 
 
-#  creation du doc
-doc = Metashape.Document()
-
-# enregistrement du projet
-# path = Metashape.app.getSaveFileName("Save Project As")
-path = r'C:\Users\U108-N806\Desktop\Comparaison mesures\Export.psx'
-try:
-    doc.save(path)
-except RuntimeError:
-    Metashape.app.messageBox("Can't save project")
-    print("Can't save project")
-
-'''
-#  creation du chunk
-chk = doc.chunk
-if not chk:
-    chk = doc.addChunk()
-'''
-
-
-# importation des photos
-PATH = r"C:\Users\U108-N806\Desktop\Comparaison mesures"
-sessionlist = os.listdir(PATH)
-for session in sessionlist:
-    if session.find("Session") == 0:
-        print(session)
-        plotlist = os.listdir(PATH + "/" + session)
-        if not os.path.exists(PATH + "/" + session + "/" + "DEMs"):
-            # Crée le fichier s'il n'existe pas
-            os.makedirs(PATH + "/" + session + "/" + "DEMs")
-        for plot in plotlist:
-            if plot.find("uplot") == 0:
-                print(plot)
-                path_session = PATH + "/" + session
-                print(path_session)
-                workflow(path_session, plot)
+boucle(sys.argv[2])
