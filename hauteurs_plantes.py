342b2d0494f1c461130ff00f5dc9633aebb23e49
aloeb <ambroise.loeb@inrae.fr>
2024-06-24 15:06:04 +0200

    update hauteurs_plantes

--- hauteurs_plantes.py
+++ hauteurs_plantes.py
@@ -40,13 +40,14 @@ def filtre_points_aberrants(matrice):
     return matrice_filtree
 
 
-def carte_hauteur_absolue(matrice, nombre_zones=100):
+def carte_hauteur_absolue(matrice, nombre_zones):
     # Taille des zones représentant n% de la matrice
     coeff = 1/math.sqrt(nombre_zones)
     zone_size = (int(matrice.shape[0] * coeff), int(matrice.shape[1] * coeff))
 
     # Initialiser les listes pour stocker les résultats
     sol_locaux = []
+
     mat_sans_nan = matrice[~np.isnan(matrice)]
     sol_bac = - np.median(np.sort(mat_sans_nan.flatten())[::-1][:int(mat_sans_nan.size * 0.03)])
     mat_hauteur = -1 * matrice.copy()
@@ -73,7 +74,7 @@ def carte_hauteur_absolue(matrice, nombre_zones=100):
     return mat_hauteur, sol_bac
 
 
-def hauteur_par_zone(matrice_h, nombre_zones=100):
+def hauteur_par_zone(matrice_h, nombre_zones):
     # Taille des zones représentant n% de la matrice
     coeff = 1 / math.sqrt(nombre_zones)
     zone_size = (int(matrice_h.shape[0] * coeff), int(matrice_h.shape[1] * coeff))
@@ -82,7 +83,7 @@ def hauteur_par_zone(matrice_h, nombre_zones=100):
     max_locals = []
     hauteurs = []
     mat_sans_nan = matrice_h[~np.isnan(matrice_h)]
-    max_glob = np.median(np.sort(mat_sans_nan.flatten())[::-1][:int(mat_sans_nan.size * 0.03)])
+    max_glob = np.median(np.sort(mat_sans_nan.flatten())[::-1][:int(mat_sans_nan.size * 0.02)])
 
     for i in range(0, matrice_h.shape[0], zone_size[0]):
         for j in range(0, matrice_h.shape[1], zone_size[1]):
@@ -91,7 +92,7 @@ def hauteur_par_zone(matrice_h, nombre_zones=100):
 
             zone_sans_nan = zone[~np.isnan(zone)]
             if zone.shape[0]*zone.shape[1] <= 0.5 * zone_size[0]*zone_size[1]:
-                hauteurs.append(np.nan)
+                hauteurs.append(np.inf)
             else:
                 mean_local = np.mean(zone_sans_nan.flatten())
                 max_local = np.median(np.sort(zone_sans_nan.flatten())[::-1][:int(zone_sans_nan.size * 0.03)])
@@ -101,12 +102,9 @@ def hauteur_par_zone(matrice_h, nombre_zones=100):
                     hauteurs.append(max_local)
                 else:
                     hauteurs.append(np.nan)
-    # plt.figure() and plt.imshow(mat_hauteur)
 
-    # Convertir les listes en tableaux numpy
-    hauteur_a = np.array(hauteurs)
-    hauteur = hauteur_a[~np.isnan(hauteur_a)]
-
+    # Representation graphique des hauteurs par zone
+    hauteur_a = np.array([int(round(h)) if not math.isinf(h) and not math.isnan(h) else (np.inf if math.isinf(h) else np.nan) for h in hauteurs])
     mat_zones_hauteur = np.zeros_like(matrice_h)
     index = 0
     for i in range(0, mat_zones_hauteur.shape[0], zone_size[0]):
@@ -114,6 +112,28 @@ def hauteur_par_zone(matrice_h, nombre_zones=100):
             # Assigner la valeur de hauteur correspondante à chaque point de la zone
             mat_zones_hauteur[i:i + zone_size[0], j:j + zone_size[1]] = hauteur_a[index]
             index += 1
-    # plt.figure() and plt.imshow(mat_zones_hauteur)
 
-    return hauteurs, mat_zones_hauteur
+    # Création de la figure et de l'axe
+    plt.ioff()  # desactive l'affichage automatique
+    figure_hauteurs, ax = plt.subplots()
+    cax = ax.imshow(mat_zones_hauteur, cmap='viridis', interpolation='none')
+    figure_hauteurs.colorbar(cax, ax=ax, label='Hauteur (mm)')
+    plt.axis('off')  # Désactiver les axes
+
+    # Ajouter les valeurs numériques sur la partie supérieure de chaque zone
+    index = 0
+    numero_z = 1
+    for i in range(0, mat_zones_hauteur.shape[0], zone_size[0]):
+        for j in range(0, mat_zones_hauteur.shape[1], zone_size[1]):
+            if not np.isinf(hauteur_a[index]):
+                ax.text(j + zone_size[1] / 10, i + zone_size[0] * 0.9, f"{numero_z}", color='red', ha='left', va='bottom', fontsize=4)
+                numero_z += 1
+                if not np.isnan(hauteur_a[index]):
+                    ax.text(j + zone_size[1] / 2, i + zone_size[0] / 2 - zone_size[0] / 4, f"{int(hauteur_a[index]):3d}", color='white', ha='center', va='center', fontsize=7)
+            index += 1
+
+    ax.set_title(f'Hauteurs maximale du couvert par zone ({nombre_zones})')
+
+    # Retourner l'objet figure
+    return hauteurs, mat_zones_hauteur, figure_hauteurs
+
