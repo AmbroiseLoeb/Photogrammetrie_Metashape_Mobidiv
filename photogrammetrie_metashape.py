ef68a9375a34350c8ad01a6fac0a340573843e37
aloeb <ambroise.loeb@inrae.fr>
2024-06-26 11:02:20 +0200

    update commentaires

--- photogrammetrie_metashape.py
+++ photogrammetrie_metashape.py
@@ -1,3 +1,4 @@
+# importation des bibliotheques
 import hauteurs_plantes
 import subprocess
 import os
@@ -6,23 +7,24 @@ import numpy as np
 import cv2 as cv
 import tkinter as tk
 from tkinter import filedialog, simpledialog
-from matplotlib import pyplot as plt
 from PIL import Image
 import csv
 from itertools import zip_longest
 from tqdm import tqdm
+from matplotlib import pyplot as plt
 
 
 def sauvegarder_image(image, path_dossier, nom_fichier):
-    """ Enregistrer une image ou une figure dans un dossier. """
+    """Enregistre une image ou une figure dans un dossier."""
 
     def figure_to_numpy(fig):
         """Convertit une figure Matplotlib en tableau numpy."""
-        fig.set_dpi(800)
+
+        fig.set_dpi(800)  # Résolution de l'image
         fig.canvas.draw()
-        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-        return cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        img_np = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        img_np = img_np.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        return cv.cvtColor(img_np, cv.COLOR_RGB2BGR)
 
     # Vérifier si le dossier existe, sinon le créer
     if not os.path.exists(path_dossier):
@@ -34,14 +36,16 @@ def sauvegarder_image(image, path_dossier, nom_fichier):
     # Vérifier que l'image est un tableau numpy
     if isinstance(image, np.ndarray):
         # Enregistrer l'image
-        cv.imwrite(chemin_complet, image)
+        cv.imwrite(str(chemin_complet), image)  # Enregistrer l'image
     else:
         image_np = figure_to_numpy(image)
-        cv.imwrite(chemin_complet, image_np)
+        cv.imwrite(str(chemin_complet), image_np)  # Enregistrer l'image
 
 
 def traiter_dossier_racine(racine_path):
-    sessionlist = os.listdir(racine_path)
+    """Traite chaque dossier session du dossier racine."""
+
+    sessionlist = os.listdir(racine_path)  # Liste des dossiers sessions dans le dossier racine
     for session in tqdm(sorted(sessionlist)):
         if session.find("Session") == 0:
             print(session)
@@ -49,14 +53,15 @@ def traiter_dossier_racine(racine_path):
 
 
 def traiter_dossier_session_ou_plot(session_or_plot_path):
+    """Traite par photogramétrie les images du dossier plot, ou de tout les dossier plot du dossier session."""
 
-    # Executer le script correspondant dans Metashape pour calculer la carte de profondeur
+    # Executer la fonction boucle du script workflow_metashape dans Metashape pour calculer la carte de profondeur
     fonction = "boucle"
     subprocess.run(
         [r'C:\Program Files\Agisoft\Metashape Pro\metashape.exe', '-r', r'workflow_metashape.py', fonction] + [
             session_or_plot_path])
 
-    # dossier d'export des DEMs
+    # Dossier d'export des DEMs
     list_dems = os.listdir(session_or_plot_path + "/" + 'DEMs')
     for file in tqdm(sorted(list_dems)):
         print(file)
@@ -65,13 +70,13 @@ def traiter_dossier_session_ou_plot(session_or_plot_path):
         dem = Image.open(session_or_plot_path + "/" + 'DEMs' + "/" + file)
         dem_array = np.array(dem)
         dem_array[dem_array <= -3276] = np.nan
-        dem_array = dem_array * 1000
+        dem_array = dem_array * 1000  # Conversion en mm
 
         # Filtre des points aberrants
         mat_filtree = hauteurs_plantes.filtre_points_aberrants(dem_array)
 
         # Calcul des hauteurs locales
-        carte_hauteur, profondeur_sol = hauteurs_plantes.carte_hauteur_absolue(mat_filtree, n_zones)
+        carte_hauteur, profondeur_sol = hauteurs_plantes.carte_hauteur_absolue(mat_filtree, n_zones)  # Carte de hauteur relative au sol
         liste_hauteurs, grille_h, figure_h = hauteurs_plantes.hauteur_par_zone(carte_hauteur, n_zones)
         print(liste_hauteurs)
 
@@ -82,19 +87,20 @@ def traiter_dossier_session_ou_plot(session_or_plot_path):
         hauteur_max = np.nanmax(liste_hauteurs)
         variance_hauteur = np.nanvar(liste_hauteurs)
         ecartype_hauteur = np.nanstd(liste_hauteurs)
-        # print(hauteur_moyenne, hauteur_mediane, hauteur_min, hauteur_max, variance_hauteur, ecartype_hauteur)
+        print(f"hauteur_moyenne : {hauteur_moyenne}\nhauteur_mediane : {hauteur_mediane}\nhauteur_min : {hauteur_min}"
+              f"\nhauteur_max : {hauteur_max}\nvariance_hauteur : {variance_hauteur}\necartype_hauteur : {ecartype_hauteur}")
 
         # Enregistrement des fichiers
-        sauvegarder_image(figure_h, session_or_plot_path, f"grille_hauteur_{file}_{n_zones}z.jpg")
+        sauvegarder_image(figure_h, session_or_plot_path, f"grille_hauteur_{file}_{n_zones}z.jpg")  # Représentation graphique des hauteurs locales dans le bac
 
-        # Export des hauteurs locales en csv
+        # Export de la liste des hauteurs en csv
         with open(os.path.basename(csv_path).replace(".csv", "_temporary.csv"), 'a', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow([os.path.basename(os.path.normpath(session_or_plot_path)) if 'Session' in os.path.basename(os.path.normpath(session_or_plot_path)) else 'N/A'] + [file] + [str(h) for h in liste_hauteurs if not math.isinf(h)])
 
 
-# lancer metashape depuis python
 def main():
+    """Fonction principale pour exécuter le pipeline."""
     global n_zones, csv_path, folder_name
 
     # Interface utilisateur pour sélectionner un dossier
@@ -102,17 +108,19 @@ def main():
     root.withdraw()
     selected_path = filedialog.askdirectory(initialdir="/home/loeb/Documents", title="Sélectionnez un dossier")
 
-    # Interface pour sélectionner le nombre de zones
+    # Interface utilisateur pour sélectionner le nombre de zones
     n_zones = simpledialog.askinteger("Nombre de zones", "Veuillez choisir un nombre de zones : \n (correspond au maillage utilisé lors de la reconnaissance du sol et des maximas locaux)", initialvalue=100, minvalue=1)
     print('nombre de zones =', n_zones)
 
     if selected_path:
         folder_name = os.path.basename(selected_path)
-        csv_path = os.path.join(selected_path, f"hauteurs_opencv_{folder_name}_{n_zones}z.csv")
+        csv_path = os.path.join(selected_path, f"hauteurs_opencv_{folder_name}_{n_zones}z.csv")  # Emplacement du fichier csv
+        # Créer le fichier csv temporaire
         with open(os.path.basename(csv_path).replace(".csv", "_temporary.csv"), 'a', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow([' '] + ['n° zone'] + [n for n in range(1, n_zones + 1)])
 
+        #  Définir le type de dossier sélectionné
         if "2024" in os.path.basename(os.path.dirname(os.path.normpath(selected_path))):  # changer 2024 par 'Session'
             print("Dossier plot sélectionné")
             traiter_dossier_session_ou_plot(selected_path)
@@ -123,14 +131,14 @@ def main():
             print("Dossier racine sélectionné")
             traiter_dossier_racine(selected_path)
 
-    # csv en ligne -> csv en colonne
+    # Passer du fichier csv en ligne au csv final en colonne
     with open(os.path.basename(csv_path).replace(".csv", "_temporary.csv"), 'r') as csvfile_temp, open(csv_path, 'w',
                                                                                                        newline='') as csvfile_final:
         csv_reader = csv.reader(csvfile_temp)
         csv_writer = csv.writer(csvfile_final)
         data_transposed = list(zip_longest(*csv_reader, fillvalue=None))
         csv_writer.writerows(data_transposed)
-    os.remove(os.path.basename(csv_path).replace(".csv", "_temporary.csv"))
+    os.remove(os.path.basename(csv_path).replace(".csv", "_temporary.csv"))  # Supprimer le fichier csv temporaire
 
 
 if __name__ == "__main__":
--- workflow_metashape.py
+++ workflow_metashape.py
@@ -1,48 +1,51 @@
-# importation librairies
+# importation des bibliotheques
 import Metashape
 import os
 import sys
 
 
 def boucle(path):
-    def workflow(path_dossier, dossier_plot=''):
-        """traitement des images jusqu'au DEM (digital elevation model)"""
+    """Fonction principale pour traiter chaque dossier plot."""
 
-        # creation du chunk
+    def workflow(path_dossier, dossier_plot=''):
+        """Traite les images par photogramétrie jusqu'au DEM (digital elevation model)."""
+
+        # Creation du chunk
         chk = doc.addChunk()
 
         # Importer les photos
         liste_images = []
         for filename in os.listdir(path_dossier + '/' + dossier_plot):
-            # vérifier si le fichier se termine par "RGB.jpg"
             if filename.endswith(".jpg"):
-                # ajouter le nom du fichier à la liste d'images
+                # Ajouter le nom du fichier à la liste d'images
                 liste_images.append(filename)
-
         for image in liste_images:
-            path_image = str(path_dossier + '/' + dossier_plot + '/' + image)
+            path_image = path_dossier + '/' + dossier_plot + '/' + image
             chk.addPhotos(path_image)
 
+        # Importer des masques pour les photos (optionnel)
         '''
         for camera in chk.cameras:
-            path_mask = str(path_dossier + '/' + dossier_plot + '/' + camera.label.split('RGB')[0] + 'MASK.jpg')
+            path_mask = str(path_dossier + '/' + dossier_plot + '/' + camera.label.split('RGB')[0] + 'MASK.png')
             chk.generateMasks(path=path_mask, masking_mode=Metashape.MaskingModeFile,
                               mask_operation=Metashape.MaskOperationReplacement, cameras=camera, tolerance=10)
         '''
 
-        # creation reference distance (avec distance camera)
+        # Créer l'échelle de référence à partir de la distance entre deux caméras
         chk.addScalebar(chk.cameras[0], chk.cameras[1])
         chk.scalebars[0].label = "dist_cam_ref"
         chk.scalebars[0].reference.distance = 0.326  # mesuré sur la nacelle
         # chk.scalebars[0].reference.distance = 0.06511302126767482  # entre camera Literal
         chk.updateTransform()
 
-        # alignement des photos
+        # Aligner les photos
         chk.matchPhotos(downscale=1, generic_preselection=True, reference_preselection=True)
         chk.alignCameras()
 
-        # delimitation de la region d'interet (bac)
-        for camera in chk.cameras:  # definir le centre des 4 cameras formant un rectangle
+        # Délimiter la region d'intérêt (le bac) :
+
+        # Définir le centre des 4 cameras formant un rectangle
+        for camera in chk.cameras:
             if 'CAM5' in camera.label:
                 c1 = camera.center
             if 'CAM4' in camera.label:
@@ -68,8 +71,7 @@ def boucle(path):
 
         # Créer la matrice de rotation
         R = Metashape.Matrix([x_axis, y_axis, z_axis]).t()  # Transposer la matrice pour l'aligner correctement
-        # Ajuster le centre de la région pour que les caméras soient aux coins supérieurs
-        bbox_center -= z_axis * (z_size / 2)
+        bbox_center -= z_axis * (z_size / 2)  # Ajuster le centre de la région pour que les caméras soient aux coins supérieurs
 
         # Définir la nouvelle région (bbox)
         region = chk.region
@@ -77,6 +79,8 @@ def boucle(path):
         region.size = Metashape.Vector([x_size, y_size, z_size])
         region.rot = R
         chk.region = region
+
+        # Convertir la région en coordonnées locales (à l'échelle) :
 
         # Récupérer le centre, la taille et la rotation de la région
         bbox_center = region.center
@@ -102,7 +106,7 @@ def boucle(path):
         chk.transform.matrix = transform_matrix_inv
         chk.updateTransform()
 
-        # construction du nuage de point
+        # Construire le nuage de point
         chk.buildDepthMaps(downscale=1, filter_mode=Metashape.MildFiltering, reuse_depth=False, max_neighbors=16,
                            subdivide_task=True, workitem_size_cameras=20, max_workgroup_size=100)
 
@@ -110,72 +114,80 @@ def boucle(path):
                             keep_depth=True, max_neighbors=10000, uniform_sampling=True, points_spacing=0.1,
                             subdivide_task=True, workitem_size_cameras=20, max_workgroup_size=100, replace_asset=False)
         '''
-        # filtre par confiance
+        Optionel : 
+        
+        # Filtre par confiance
         chk.point_cloud.setConfidenceFilter(0, 1)
         all_points_classes = list(range(128))
         chk.point_cloud.removePoints(all_points_classes)  # removes all active points of the point cloud, i.e. removing all low-confidence points
         chk.point_cloud.resetFilters()  # resetting filter, so high-confidence points are now active
 
-        # filtre par couleur
+        # Filtre par couleur
         gray_color = [84, 66, 52]
         tolerance = 25
         chk.point_cloud.selectPointsByColor(gray_color, tolerance, channels='RGB')
         chk.point_cloud.assignClassToSelection(7)
-        '''
-
-        '''
-        # classification sol
+        
+        # Classification du sol
         chk.point_cloud.classifyGroundPoints(max_angle=30, max_distance=0.01, max_terrain_slope=10, cell_size=0.5)
-        # chk.point_cloud.setClassesFilter(Metashape.Ground)
+        # chk.point_cloud.setClassesFilter(Metashape.Ground)  # Affiche les points de sol
         '''
 
-        doc.save()
-        # construction du modele numerique d'elévation (DEM)
+        doc.save()  # Sauvegarde du projet
+
+        # Construire le modele numerique d'elévation (DEM) :
+
         # DEM toutes classes
         chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.DisabledInterpolation)
         chk.elevation.label = "DEM"
         key_allclass = chk.elevation.key
 
         """
-        # DEM sol
+        # DEM sol, si classification des points de sol
         chk.buildDem(source_data=Metashape.PointCloudData, interpolation=Metashape.Extrapolated, classes=Metashape.PointClass.Ground)
-        chk.elevation.label = "Elevation sol"
+        chk.elevation.label = "DEM_sol"
         key_sol = chk.elevation.key
+        
+        # DEM sol, si import d'une DEM sol existante
+        chk.importRaster(path='path_vers_DEM_sol.tif', raster_type=Metashape.ElevationData, nodata_value=-32767)
+        chk.elevation.active = True  # S'assurer que la DEM est active
+        chk.elevation.label = "DEM_sol"
+        key_sol = chk.elevation.key
+
         # DEM finale
         chk.transformRaster(source_data=Metashape.ElevationData, asset=key_allclass, subtract=True, operand_asset=key_sol)
         chk.elevation.label = "DEM_finale"
         """
 
-        # exportation du DEM
-        if len(dossier_plot) > 1:
+        # Export de la DEM
+        if len(dossier_plot) > 1:  # Si dossier session
             label_dem = chk.elevation.label + '_' + dossier_plot
-        else:
-            label_dem = chk.elevation.label + '_' + os.path.basename(os.path.normpath(path))
+        else:  # Si dossier plot
+            label_dem = chk.elevation.label + '_' + os.path.basename(os.path.normpath(path_dossier))
         doc.save()
         chk.exportRaster(path_dossier + '/' + "DEMs" '/' + label_dem + '.tif',
                          source_data=Metashape.ElevationData)
 
-    #  creation du doc
+    # Créer un nouveau document projet Metashape
     doc = Metashape.Document()
 
-    # enregistrement du projet
-    # path = Metashape.app.getSaveFileName("Save Project As")
+    # Enregistrer le projet
+    # path = Metashape.app.getSaveFileName("Save Project As")  # Laisser à l'utilisateur le choix du nom et de l'emplacement du projet
     path_project = path + r'\export.psx'
     doc.save(path_project)
 
-    # créer le dossier d'export des DEMs
+    # Créer le dossier d'export des DEMs
     if not os.path.exists(path + "/" + "DEMs"):
-        # Crée le fichier s'il n'existe pas
-        os.makedirs(path + "/" + "DEMs")
+        os.makedirs(path + "/" + "DEMs")  # Crée le fichier s'il n'existe pas
 
-    if 'Session' in os.path.basename(os.path.normpath(path)):
+    if 'Session' in os.path.basename(os.path.normpath(path)):  # Si le dossier est un dossier session
         print(os.path.basename(os.path.normpath(path)))
         plotlist = os.listdir(path)
-        for plot in plotlist:
+        for plot in plotlist:  # Traiter dans le même projet tout les dossiers plots de la session
             if plot.find("1") == 0:
                 print(plot)
                 workflow(path, plot)
-    else:
+    else:  # Si le dossier est un dossier plot
         print(os.path.basename(os.path.normpath(path)))
         workflow(path)
 
